8423fd5859f4dc56b03072617c31d97175936b71
Jonas Laursen <jola@cloudfactory.dk>
2019-02-20 13:49:19 +0100

    Add project files.

---
+++ DataSearchEngine/Program.cs
@@ -0,0 +1,76 @@
+﻿using RestSharp;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DataSearchEngine {
+	class Program {
+		private static int Counter = 0;
+
+		private static readonly object _lock = new object();
+		static void Main(string[] args) {
+			Program p = new Program();
+
+			while (true) {
+				Counter = 0;
+				Console.WriteLine("Hello!");
+
+				Console.WriteLine("Please input search string:");
+				var input = Console.ReadLine();
+
+				Console.WriteLine("Please hold while searching...");
+				var inputs = input.Split(' ');
+
+				p.ProcessDirectory("data/", inputs);
+
+				Console.WriteLine("Done, Results: " + Counter.ToString());
+				Console.ReadLine();
+				Console.Clear();
+			}
+		}
+
+		public void ProcessDirectory(string targetDirectory, string[] inputs) {
+			// Process the list of files found in the directory.
+			string[] fileEntries = Directory.GetFiles(targetDirectory);
+			foreach(var fileName in fileEntries) {
+				ProcessFile(fileName, inputs);
+			}
+
+			// Recurse into subdirectories of this directory.
+			string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+			foreach(var subdirectory in subdirectoryEntries) {
+				ProcessDirectory(subdirectory, inputs);
+			}
+		}
+
+		// Insert logic for processing found files here.
+		public static void ProcessFile(string path, string[] inputs) {
+			var client = new RestClient("http://localhost:29697/api");
+
+			var request = new RestRequest("main");
+
+			var model = new SearchForModel() {
+				path = Directory.GetCurrentDirectory() + "/" + path,
+				input = inputs
+			};
+
+			request.RequestFormat = DataFormat.Json;
+			request.AddJsonBody(model);
+
+			var response = client.Execute(request, Method.POST);
+
+			bool found = bool.Parse(response.Content);
+			if (!found) return;
+
+			lock(_lock) {
+				Counter++;
+			}
+			Console.WriteLine("Found in " + path);
+		}
+
+		public class SearchForModel {
+			public string path { get; set; }
+			public string[] input { get; set; }
+		}
+	}
+}
---
+++ ProcessFileService/Controllers/MainController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.IO;
+
+namespace ProcessFileService.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MainController : ControllerBase {
+		// POST api/values
+		[HttpPost]
+		public bool Post([FromBody] SearchForModel model) {
+
+			var keywordfound = false;
+			foreach (var line in System.IO.File.ReadAllLines(model.path)) {
+				foreach (var a in model.input) {
+					if (line.Contains(a))
+						keywordfound = true;
+				}
+			}
+
+			if (!keywordfound) return false;
+
+			return true;
+		}
+	}
+
+	public class SearchForModel {
+		public string path { get; set; }
+		public string[] input { get; set; }
+	}
+
+}
