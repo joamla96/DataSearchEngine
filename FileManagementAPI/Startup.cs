9d1637308b4ed6c0ea103316ca73bcfd3b53f88b
Jonas Laursen <joamla96@gmail.com>
2019-03-04 14:58:17 +0100

    Disabled https redirection because docker issues

--- FileManagementAPI/Startup.cs
+++ FileManagementAPI/Startup.cs
@@ -34,10 +34,10 @@ namespace FileManagementAPI {
 				app.UseDeveloperExceptionPage();
 			} else {
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
+				//app.UseHsts();
 			}
 
-			app.UseHttpsRedirection();
+			//app.UseHttpsRedirection();
 			app.UseMvc();
 		}
 	}
