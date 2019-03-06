7ca1a836b1d8da11d512290b12ed0a7d61b63ff0
Jonas Laursen <joamla96@gmail.com>
2019-03-06 11:31:40 +0100

    Created loggin instance

---
+++ Logger/LogEntryDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger {
+	public class LogEntryDTO {
+		public string ServiceName;
+		public string Message;
+	}
+}
